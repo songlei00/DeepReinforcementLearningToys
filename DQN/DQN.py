b41cf62e3e56b5503474cca4282a2e095ab758fa
songlei <974534426@qq.com>
2020-07-13 23:39:00 +0800

    2020年 07月 13日 星期一 23:39:00 CST

--- DQN/DQN.py
+++ DQN/DQN.py
@@ -5,17 +5,18 @@ from torch import nn, optim
 import torch.nn.functional as F
 import random
 import numpy as np
+import matplotlib.pyplot as plt
 
 class ReplayBuffer():
 
     def __init__(self, buf_sz, batch_sz=1, seed=None):
         self.memory = deque(maxlen=buf_sz)
         self.batch_sz = batch_sz
-        self.experience = namedtuple("Experience", field_names=['state', 'action', 'reward', 'next_state', 'done'])
+        self.experience = namedtuple("Experience", field_names=['state', 'action', 'reward', 'next_state'])
         random.seed(seed)
 
-    def add_experience(self, s, a, r, s_, done):
-        experience = self.experience(s, a, r, s_, done)
+    def add_experience(self, s, a, r, s_):
+        experience = self.experience(s, a, r, s_)
         self.memory.append(experience)
 
     def random_select(self, n=None):
@@ -27,11 +28,11 @@ class ReplayBuffer():
 
 class QNetwork(nn.Module):
 
-    def __init__(self, input_sz=4):
+    def __init__(self, input_sz=4, output_sz=2):
         super(QNetwork, self).__init__()
         self.fc1 = nn.Linear(input_sz, 120)
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 1)
+        self.fc3 = nn.Linear(84, output_sz)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
@@ -46,20 +47,26 @@ class DQN():
         env,
         n_action,
         gamma=0.99,
-        epsilon=1.0,
-        max_step=None
+        epsilon=0.1,
+        target_update=100,
+        max_step=10000
     ):
         self.env = env
         self.n_action = n_action
         self.gamma = gamma
         self.epsilon = epsilon
+        self.target_update = target_update
+        self.max_step = max_step
 
         self.replay_buffer = ReplayBuffer(200)
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        self.q_net = QNetwork().to(self.device)     # q eval
-        self.q_target = QNetwork().to(self.device)
-        self.optimizer = torch.optim.Adam(self.q_net.parameters())
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.q_net = QNetwork()    # q eval
+        self.q_target = QNetwork()  # fixed
+        self.q_target.load_state_dict(self.q_net.state_dict())
+        self.opt = torch.optim.RMSprop(self.q_net.parameters())
         self.loss_fn = nn.MSELoss()
+
+        self.reward_list = []
         
 
     def select_action(self, state):
@@ -68,34 +75,58 @@ class DQN():
             a = np.random.randint(0, self.n_action)
         else:
             with torch.no_grad():
-                self.q_net(state)
+                a = self.q_net(state).max(1)[1].numpy()[0]
+        return a
 
 
-    def step(self):
+    def step(self, episode):
         s = self.env.reset()
-        while True:
+        tot_r = 0
+        for i in range(self.max_step):
             self.env.render()
             a = self.select_action(s)
             s_, r, done, _ = self.env.step(a)
-            self.replay_buffer.add_experience(s, a, r, s_, done)
+            tot_r += r
+            self.replay_buffer.add_experience(s, a, r, s_)
             s = s_
             self.learn()
             if done:
                 break
 
+            if episode % self.target_update == 0:
+                self.q_target.load_state_dict(self.q_net.state_dict())
+
+        self.reward_list.append(tot_r)
+
     def learn(self):
-        s, a, r, s_, done = self.replay_buffer.random_select()
-        with torch.no_grad():
-            q_target_val = 
+        ret = self.replay_buffer.random_select()
+        s, a, r, s_ = ret[0]
+        s = torch.tensor(s, dtype=torch.float32).unsqueeze(0)
+        q_val = self.q_net(s)
+        s_ = torch.tensor(s_, dtype=torch.float32).unsqueeze(0)
+        q_target_val = self.q_target(s_)
+        epected_q = q_target_val * self.gamma + r
+        loss = self.loss_fn(q_val, epected_q)
+        self.opt.zero_grad()
+        loss.backward()
+        for param in self.q_net.parameters():
+            param.grad.data.clamp_(-1, 1)
+        self.opt.step()
+
+    def plot_reward(self):
+        plt.plot(self.reward_list)
+        plt.show()
 
 
 env = gym.make('CartPole-v0')
 dqn = DQN(env, 2)
 
-for episode in range(10):
-    dqn.step()
+for episode in range(10000):
+    dqn.step(episode)
 
 env.close()
+
+dqn.plot_reward()
 
 
 # env = gym.make('CartPole-v0')
